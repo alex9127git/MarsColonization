39ef1dc352b5204d256de702a5b9255684bc7a3c
alex9127git <mail.alex9127@yandex.ru>
2023-02-06 18:14:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, url_for, request
+i = 0
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    return "Миссия Колонизация Марса"
+
+
+@app.route("/index")
+def index():
+    return "И на Марсе яблони будут цвести!"
+
+
+@app.route("/promotion")
+def promotion():
+    return "</br>".join(("Человечество вырастает из детства.",
+                         "Человечеству мала одна планета.",
+                         "Мы сделаем обитаемыми безжизненные пока планеты.",
+                         "И начнем с Марса!",
+                         "Присоединяйся!"))
+
+
+@app.route("/image_mars")
+def image_mars():
+    with open("static/html/image_mars.html", "r", encoding="utf-8") as file:
+        return file.read()
+
+
+@app.route("/promotion_image")
+def promotion_image():
+    with open("static/html/promotion_image.html", "r", encoding="utf-8") as file:
+        return file.read()
+
+
+@app.route("/registration_form")
+def registration_form():
+    with open("static/html/form.html", "r", encoding="utf-8") as file:
+        return file.read()
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
