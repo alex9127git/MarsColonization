1669fe0e59f49469284efc707a7d543e55ed1d79
alex9127git <mail.alex9127@yandex.ru>
2023-02-20 20:16:55 +0300

    Added page that displays register form answers

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 # -*- encoding: utf-8 -*-
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import db_session
 
 
 app = Flask(__name__)
+answers_param = dict()
 
 
 @app.route("/")
 @app.route("/index")
 def root():
-    return render_template('base.html', title='Домашняя страница')
+    return render_template('base.html', title='Домашняя страница',
+                           stylesheet_path=url_for('static', filename='css/style.css'))
 
 
 @app.route("/list_prof/<list_type>")
 def list_prof(list_type):
-    return render_template('profession_list.html', title='Список профессий', list_type=list_type)
+    return render_template('profession_list.html', title='Список профессий', list_type=list_type,
+                           stylesheet_path=url_for('static', filename='css/style.css'))
 
 
 @app.route("/promotion")
@@ -39,10 +42,33 @@ def promotion_image():
         return file.read()
 
 
-@app.route("/registration_form")
-def registration_form():
-    with open("static/html/form.html", "r", encoding="utf-8") as file:
-        return file.read()
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    global answers_param
+    if request.method == 'GET':
+        return render_template('register.html', title='Анкета претендента на участие в миссии',
+                               stylesheet_path=url_for('static', filename='css/style.css'))
+    elif request.method == 'POST':
+        answers_param = {
+            "name": request.form.get('name'),
+            "surname": request.form.get('surname'),
+            "email": request.form.get('email'),
+            "education": request.form.get('education'),
+            "engineer": request.form.get('engineer') is not None,
+            "pilot": request.form.get('pilot') is not None,
+            "builder": request.form.get('builder') is not None,
+            "nurse": request.form.get('nurse') is not None,
+            "sex": request.form.get('sex'),
+            "motive": request.form.get('motive'),
+            "ready": request.form.get('accept') is not None
+        }
+        return redirect("/answer")
+
+
+@app.route("/answer")
+def answer():
+    global answers_param
+    return render_template('answer.html')
 
 
 @app.route("/load_photo", methods=["GET", "POST"])
